3aeaa151d9e58035dead1307bbc68e391cbb7997
Hadrien Devichi <h.devichi@wanadoo.fr>
2016-06-01 11:37:49 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webSandboxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webSandboxApp
+ */
+angular.module('webSandboxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
